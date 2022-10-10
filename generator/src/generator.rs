0da2d4475e1219ff36e3c25f39a6ce8b98d545c5
Nicolas Trüssel <nicolas+git@ntruessel.ch>
2022-10-10 20:40:04 +0200

    Implement File Generation

--- generator/src/generator.rs
+++ generator/src/generator.rs
@@ -1,8 +1,8 @@
 use handlebars::Handlebars;
 use crate::model::Recipe;
 
-pub fn generate_recipe(r: &Recipe) {
+pub fn generate_recipe(r: &Recipe) -> String {
     let mut templates = Handlebars::new();
     templates.register_template_string("recipe", include_str!("../templates/recipe.handlebars")).expect("Failed to load templates");
-    println!("{}", templates.render("recipe", r).expect("Failed to render recipe"));
+    templates.render("recipe", r).expect("Failed to render recipe")
 }
--- generator/src/main.rs
+++ generator/src/main.rs
@@ -1,13 +1,21 @@
 use std::fs;
+use std::path::Path;
 use crate::generator::generate_recipe;
 
 mod model;
 mod generator;
 
 fn main() {
-    let paths = fs::read_dir("./recipes").unwrap();
+    let _ = fs::remove_dir_all("../public");
+    fs::create_dir("../public").unwrap();
+    let paths = fs::read_dir("../content").unwrap();
     for path in paths {
-        let recipe = serde_yaml::from_str(fs::read_to_string(path.unwrap().path()).unwrap().as_str()).unwrap();
-        generate_recipe(&recipe);
+        let directory = path.unwrap();
+        let recipe_file = directory.path().join("recipe.yaml");
+        let recipe = serde_yaml::from_str(fs::read_to_string(recipe_file).unwrap().as_str()).unwrap();
+        let content = generate_recipe(&recipe);
+        let mut outpath = Path::new("../public").join(directory.file_name());
+        outpath.set_extension("html");
+        fs::write(outpath, content).unwrap();
     }
 }
