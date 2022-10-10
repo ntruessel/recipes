1df69dd0b4f24524e57592c53aafd17fe7f57c52
Nicolas Trüssel <nicolas+git@ntruessel.ch>
2022-10-10 21:16:46 +0200

    Fix Directory Structure

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,15 @@ mod model;
 mod generator;
 
 fn main() {
-    let _ = fs::remove_dir_all("../public");
-    fs::create_dir("../public").unwrap();
-    let paths = fs::read_dir("../content").unwrap();
+    let _ = fs::remove_dir_all("public");
+    fs::create_dir("public").unwrap();
+    let paths = fs::read_dir("content").unwrap();
     for path in paths {
         let directory = path.unwrap();
         let recipe_file = directory.path().join("recipe.yaml");
         let recipe = serde_yaml::from_str(fs::read_to_string(recipe_file).unwrap().as_str()).unwrap();
         let content = generate_recipe(&recipe);
-        let mut outpath = Path::new("../public").join(directory.file_name());
+        let mut outpath = Path::new("public").join(directory.file_name());
         outpath.set_extension("html");
         fs::write(outpath, content).unwrap();
     }
