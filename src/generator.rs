08cf9d087768335f1388361316d3adf6bdddbd80
Nicolas Trüssel <nicolas+git@ntruessel.ch>
2022-10-11 22:06:49 +0200

    Create Index Page

--- src/generator.rs
+++ src/generator.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use handlebars::Handlebars;
 use crate::model::Recipe;
 
@@ -5,4 +6,11 @@ pub fn generate_recipe(r: &Recipe) -> String {
     let mut templates = Handlebars::new();
     templates.register_template_string("recipe", include_str!("../templates/recipe.handlebars")).expect("Failed to load templates");
     templates.render("recipe", r).expect("Failed to render recipe")
+}
+
+pub fn generate_index(recipes: &Vec<String>) -> String {
+    let mut templates = Handlebars::new();
+    let data = HashMap::from([("recipes", recipes)]);
+    templates.register_template_string("index", include_str!("../templates/index.handlebars")).expect("Failed to load templates");
+    templates.render("index", &data).expect("Failed to render index")
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 use std::path::Path;
-use crate::generator::generate_recipe;
+use crate::generator::{generate_index, generate_recipe};
 
 mod model;
 mod generator;
@@ -9,8 +9,10 @@ fn main() {
     let _ = fs::remove_dir_all("public");
     fs::create_dir("public").unwrap();
     let paths = fs::read_dir("content").unwrap();
+    let mut recipes = Vec::new();
     for path in paths {
         let directory = path.unwrap();
+        recipes.push(directory.file_name().to_str().unwrap().to_string());
         let recipe_file = directory.path().join("recipe.yaml");
         let recipe = serde_yaml::from_str(fs::read_to_string(recipe_file).unwrap().as_str()).unwrap();
         let content = generate_recipe(&recipe);
@@ -18,4 +20,7 @@ fn main() {
         outpath.set_extension("html");
         fs::write(outpath, content).unwrap();
     }
+    let index = generate_index(&recipes);
+    let outpath = Path::new("public/index.html");
+    fs::write(outpath, index).unwrap();
 }
